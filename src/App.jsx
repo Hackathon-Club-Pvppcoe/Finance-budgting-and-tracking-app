5463336eed5c9000d5016f809a50e34596618b15
Kartikey-Sharma108 <skartikeydjjdjj@gmail.com>
2026-02-18 12:12:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+
+  return (
+    <MantineProvider>
+    <>
+     <h1>welcome to our repository</h1>
+    </>
+    </MantineProvider>
+  )
+}
+
+export default App
